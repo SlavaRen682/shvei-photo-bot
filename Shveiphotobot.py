86038abe0fbfd79a26b7eb7e9bf5d531be8415ca
SlavaRen682 <s.gabanyan05@mail.ru>
2025-07-15 02:25:18 +0500

    Обновления бота

--- Shveiphotobot.py
+++ Shveiphotobot.py
@@ -154,4 +154,5 @@ def choose_category(call):
 if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=WEBHOOK_URL)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), threaded=True)
+
